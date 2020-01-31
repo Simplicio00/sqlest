f1c7c7c7142beeda62b3cb0b56974972059cfc82
FIC <FIC@info.local>
2020-01-31 16:09:03 -0300

    v-a2 bd sql server h7

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -40,7 +40,6 @@ Permissao VARCHAR(200) NOT NULL
 
 
 
-
 -- DML MANIPULAÇÃO DE DADOS
 
 -- alter table tabela, drop column coluna
@@ -59,7 +58,10 @@ VALUES('Equalize','25/08/2001','ACRE',120, 3,3),
 ('album2','25/01/1970','Rio Branco', 350,1,1),
 ('album2','25/01/1970','Rio Branco', 350,4,4);
 
+INSERT INTO Album(Nome, DataLancamento, Localizacao, QtdMinutos, IdArtista, IdEstilo)
+VALUES('Carambolas','25/01/1989','Macapá', 350,1,1);
 
+SELECT * FROM Album;
 
 --update para alterar dados
 
@@ -85,4 +87,49 @@ SELECT Nome from Album;
 SELECT Nome, DataLancamento FROM Album;
 
 SELECT * FROM Album WHERE IdAlbum=1;
+
+SELECT * FROM Album WHERE IdAlbum >1;
+
+SELECT * FROM Album WHERE IdAlbum >= 1;
+
+
+-- OR // OU 
+
+--filtrar por caso nulo
+
+SELECT Nome, QtdMinutos FROM Album
+WHERE (DataLancamento IS NULL) OR (Localizacao IS NULL);
+
+-- consultar dado por conteúdo no inicio 
+
+SELECT IdAlbum, Nome FROM Album
+WHERE Nome LIKE 'Album2%';
+-- WHERE Nome LIKE '%Album2%'; pega o conteúdo no meio da frase
+
+
+--ORDENAÇÃO
+SELECT Nome from Album order by Nome;
+
+SELECT IdAlbum, Nome, QtdMinutos FROM Album
+order by QtdMinutos;
+
+-- Ordenaçao invertida ou decrescente 
+SELECT IdAlbum, Nome, QtdMinutos FROM Album
+order by QtdMinutos DESC;
+-- ascendente
+--SELECT IdArtista, Nome from Artistas AND IdAlbum, Nome, QtdMinutos, DataLancamento FROM Album
+--order by DataLancamento ASC;
+
+
+-- Contagem
+SELECT COUNT(IdAlbum) FROM Album;
+
+SELECT IdAlbum, IdArtista, Nome FROM Album
+WHERE IdArtista=1;
+
+SELECT IdAlbum, Nome, DataLancamento FROM Album
+WHERE DataLancamento LIKE '%1970-01-25%';
+
+SELECT Nome, Datalancamento, IdArtista from Album
+order by DataLancamento ASC; 
 
