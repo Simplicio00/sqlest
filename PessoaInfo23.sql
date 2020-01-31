76eb1088a4d2921ab7ccacbdf7085a50d1b7a9b9
FIC <FIC@info.local>
2020-01-31 17:18:52 -0300

    v-a2 bd sql server h9

---
+++ PessoaInfo23.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE PessoaInfo_Tarde;
+
+USE PessoaInfo_Tarde;
+
+
+
+
+CREATE TABLE Telefones(
+Id_Telefone int identity primary key,
+Numero_Telefone int not null
+);
+
+CREATE TABLE Emails(
+Id_Email int identity primary key,
+Nome_Email varchar(50) NOT NULL
+);
+
+CREATE TABLE CNH(
+Id_CNH int identity primary key,
+Habilitacao bit NOT NULL
+);
+
+
+CREATE TABLE Pessoas(
+Id_Pessoa int identity primary key,
+Nome_Pessoa VARCHAR(100) NOT NULL,
+-- estrangeiras
+Id_Telefone int foreign key references Telefones(Id_Telefone),
+Id_Email int foreign key references Emails(Id_Email),
+Id_CNH int foreign key references CNH(Id_CNH)
+);
+
+
+
+INSERT INTO Telefones(Numero_Telefone)
+VALUES(9090-8716),(9090-8677);
+
+INSERT INTO Emails(Nome_Email)
+VALUES('carlinhos@sd.com'),('GabiSousa@dalls.com');
+
+INSERT INTO CNH(Habilitacao)
+VALUES(1),(0);
+
+INSERT INTO Pessoas(Nome_Pessoa, Id_Telefone, Id_Email, Id_CNH)
+VALUES('Carlos Augusto Ferreira',1,1,1),('Gabriela Brás de Sousa',2,2,2);
+
+SELECT Pessoas.Nome_Pessoa, Telefones.Numero_Telefone FROM Pessoas 
+INNER JOIN Telefones ON Telefones.Id_Telefone = Pessoas.Id_Telefone; 
+
+SELECT Pessoas.Nome_Pessoa, Emails.Nome_Email FROM Pessoas
+INNER JOIN Emails ON Emails.Id_Email = Pessoas.Id_Email;
+
+
+SELECT Pessoas.Nome_Pessoa, CNH.Habilitacao FROM Pessoas
+INNER JOIN CNH ON CNH.Id_CNH = Pessoas.Id_CNH;
--- livrosExercicio.sql
+++ livrosExercicio.sql
@@ -1,11 +1,7 @@
---COMENTARIO
---CRIAR BANCO DE DADOS
 CREATE DATABASE Biblioteca_Tarde;
 
--- Apontar para o banco que quero usar
 USE Biblioteca_Tarde;
 
--- CRIAR TABELAS 
 CREATE TABLE Autores (
 	IdAutor     INT PRIMARY KEY IDENTITY,
 	NomeAutor	VARCHAR(200) NOT NULL
@@ -13,7 +9,7 @@ CREATE TABLE Autores (
 
 CREATE TABLE Generos (
 	IdGenero	INT PRIMARY KEY IDENTITY,
-	Nome		VARCHAR(200) NOT NULL
+	NomeGenero	VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Livros (
@@ -22,30 +18,34 @@ CREATE TABLE Livros (
 	IdAutor		INT FOREIGN KEY REFERENCES Autores (IdAutor),
 	IdGenero	INT FOREIGN KEY REFERENCES Generos (IdGenero)
 );
-
---SELECIONAR TUDO DAS SEGUINTES TABELAS 
+ 
 SELECT * FROM Generos;
 SELECT * FROM Autores;
 SELECT * FROM Livros;
 
--- ALTERAR ADICIONAR UMA NOVA COLUNA
-ALTER TABLE Generos
-ADD Descricao VARCHAR(255);
+SELECT * FROM Autores;
 
--- ALTERAR TIPO DE DADO
-ALTER TABLE Generos
-ALTER COLUMN Descricao CHAR(100);
+INSERT INTO Autores(NomeAutor)
+VALUES('Miguel'),('João'),('Venâncio');
 
---ALTERAR EXCLUIR COLUNA 
-ALTER TABLE Generos
-DROP COLUMN Descricao;
+SELECT * FROM Generos;
+INSERT INTO Generos(NomeGenero)
+VALUES('Suspense/Noir'),('Romance'),('Drama'),('Terror');
 
--- Excluir tabela 
-DROP TABLE Livros;
-DROP TABLE Generos;
-DROP TABLE Autores;
+SELECT * FROM Livros;
+INSERT INTO Livros(titulo,IdAutor,IdGenero)
+VALUES('Do outro lado',1,1),('Cães também amam',2,2),('Um dia na guerra',3,3),('A casa assombrada',4,4);
 
--- Excluir banco de dados 
-DROP DATABASE Biblioteca_Tarde;
+SELECT NomeAutor from Autores;
+
+SELECT NomeGenero from Generos;
+
+SELECT Titulo from Livros;
+
+SELECT Livros.Titulo, Autores.NomeAutor FROM Livros 
+JOIN Autores ON Livros.IdAutor = Autores.IdAutor;
 
 
+SELECT Livros.Titulo, Generos.NomeGenero FROM Livros 
+JOIN Generos ON Livros.IdGenero = Generos.IdGenero 
+JOIN Autores ON Livros.IdAutor = Autores.IdAutor;
